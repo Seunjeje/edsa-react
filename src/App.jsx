7d7af3106aff7c03873156402a822c57feaf7a9e
Seunjeje <oluwaseunjejelaye95@gmail.com>
2023-05-05 15:04:38 +0100

    Edit src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
 import { React, useState } from "react";
-import { Routes, Route, Link, Outlet } from "react-router-dom";
-import CurrencySwitcher from "./Components/CurrencySwitcher";
-import Display from "./Components/Display";
-import Home from "./Components/Home";
-import Form from "./Components/Form";
+
+import CurrencySwitcher from "./Components/CurrencySwitcher"
+
+import Display from "./Components/Display"
 
 import "./App.css";
 
 function App() {
-  // const [currency, setCurrency] = useState("EUR");
 
-  // const handleCurrencyChange = () => {
-  //   //TODO: using traditional if else statement determine the new currency
-  //   setCurrency(currency === "EUR" ? "USD" : "EUR");
-  // };
+  const [currency, setCurrency] = useState("EUR");
+
+  const handleCurrencyChange = () => {
+
+    if (currency === "EUR") {
+
+      setCurrency("USD");
+
+    } else {
+
+      setCurrency;
+
+    }
+
+    ("EUR");
+
+  };
+
+
+
 
   return (
+
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="form">Form</Link>
-      </nav>
-      <h1>This is a basic Router Setup
-      </h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="form" element={<Form />} />
-      </Routes>
+
+      <Display currency={currency} />
+
+      <CurrencySwitcher
+
+        handleChangeCurrency={handleCurrencyChange}
+
+        currency={currency}
+
+      />
+
     </div>
+
   );
+
 }
 
+
+
+
 export default App;
+
+
+
+
+
